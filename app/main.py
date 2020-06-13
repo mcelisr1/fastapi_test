849194911b66acc7aa4f8fe5972e470edd257bdd
Miguel Celis <miguel@heippi.com>
2020-06-13 10:11:43 -0500

    Sesión 13/06/20: Añadidas dos rutas con de utilización de función

--- app/main.py
+++ app/main.py
@@ -8,29 +8,34 @@ def read_hola():
     return {"mensaje": "Hola Leidy"}
 
 
-@app.get("/realizar_flexiones/")
-def realizar_flexiones():    
-    # iniciar_flexiones = False
-    
-    # Entradas
-    # A. Identificar acción para iniciar las flexiones
-    iniciar_flexiones = True # Boolean
+@app.get("/no_ha_hecho_flexiones/")
+def no_ha_hecho_flexiones():
+    respuesta_de_funcion = ejecutar_flexiones(iniciar_flexiones=True)
 
-    # B. Cantidad de flexiones a realizar
-    flexiones_a_realizar = 20
+    return {"mensaje": respuesta_de_funcion}
 
+
+@app.get("/ya_hizo_flexiones/")
+def ya_hizo_flexiones():
+    respuesta_de_funcion = ejecutar_flexiones(iniciar_flexiones=False, flexiones_a_realizar=0)
+
+    return {"mensaje": respuesta_de_funcion}
+
+
+def ejecutar_flexiones(iniciar_flexiones, flexiones_a_realizar=20):
     if iniciar_flexiones is True:
         # Procesamiento
         # Contador iniciado en cero
         contador_de_flexiones = 0
 
         # B. Aumentar en 1 el acumulador por cada flexión realizada
-        while contador_de_flexiones < 20:
+        while contador_de_flexiones < flexiones_a_realizar:
             contador_de_flexiones += 1
             print(f'Flexión número: {contador_de_flexiones}')
 
     elif iniciar_flexiones is False:
-        return {"mensaje": f"Descansar"}
+        return "Descansar"
 
     # Salida
-    return {"mensaje": f"{flexiones_a_realizar} Flexiones realizadas"}
+    return f"{flexiones_a_realizar} Flexiones realizadas"
+
