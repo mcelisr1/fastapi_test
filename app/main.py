cf35041bbdd1ec49442bbb659dc45d52bf4a1d00
Miguel Celis <miguel@heippi.com>
2020-06-26 19:40:01 -0500

    Utilización de datos simples y estructurados con Listas y Tuplas

--- app/main.py
+++ app/main.py
@@ -8,6 +8,52 @@ def read_hola():
     return {"mensaje": "Hola Leidy"}
 
 
+@app.get("/explorando_variables")
+def explorando_variables():
+    edad = 18
+    nombre = 'Juanita'
+    es_mujer = True
+    estatura = 1.58
+    comidas_favoritas = ['Arepa de Huevo', 'Mote de queso', 'Posta Cartagenera']
+
+    print(f'Nombre: {nombre} Edad: {edad} Mujer: {es_mujer} Estatura: {estatura}')    
+    print(f'Comidas favoritas: {comidas_favoritas}')
+
+    print(f'Segunda Comida favorita: {comidas_favoritas[1]}')
+    comidas_favoritas.append('Carimañola')    
+    print(f'Comidas favoritas: {comidas_favoritas}')
+    comidas_favoritas.insert(2, 'Buñuelo')
+    print(f'Comidas favoritas: {comidas_favoritas}')
+    print(f'Slice: {comidas_favoritas[1:3]}')
+    print(f'Slice -2: {comidas_favoritas[:-2]}')
+    otras_comidas = ['Arroz de coco', 'Sopa de Mondongo']
+    comidas_favoritas.extend(otras_comidas)    
+    print(f'Comidas favoritas: {comidas_favoritas}')
+    comidas_favoritas.pop(0)
+    print(f'Comidas favoritas: {comidas_favoritas}')
+    comidas_favoritas.remove('Buñuelo')
+    print(f'Comidas favoritas: {comidas_favoritas}')
+
+    print(f'Tipos: {type(nombre)} {type(edad)} {type(es_mujer)} {type(estatura)} {type(comidas_favoritas)}')
+
+    edad = 'Veinte'
+    nombre = 'Juanchito'
+    es_mujer = 'No'
+    estatura = 168
+    comidas_favoritas = ('Bandeja paisa', 'Ajiaco', 'Tamal')
+
+    print(f'Nombre: {nombre} Edad: {edad} Mujer: {es_mujer} Estatura: {estatura}')
+    print(f'Comidas favoritas: {comidas_favoritas}')
+
+    print(f'Tercera Comida favorita: {comidas_favoritas[2]}')
+    print(f'Slice: {comidas_favoritas[0:2]}')
+    print(f'Slice -2: {comidas_favoritas[:-2]}')
+
+    print(f'Tipos: {type(nombre)} {type(edad)} {type(es_mujer)} {type(estatura)} {type(comidas_favoritas)}')
+
+    return {"mensaje": "Variables exploradas"}
+
+
 @app.get("/no_ha_hecho_flexiones/")
 def no_ha_hecho_flexiones():
     respuesta_de_funcion = ejecutar_flexiones(iniciar_flexiones=True)
